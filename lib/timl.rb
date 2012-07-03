90d1cbcbcee2df58d872c3b7fd7faf0c49665698
Sam Rose <samwho@lbak.co.uk>
2012-07-03 14:45:42 +0100

    Added license, bumped version number.

--- lib/timl.rb
+++ lib/timl.rb
@@ -2,7 +2,7 @@ libdir = File.dirname(__FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 
 module Timl
-  VERSION   = '0.1'
+  VERSION   = '0.2'
   ROOTDIR   = File.expand_path(File.dirname(__FILE__) + '/..')
   SPECDIR   = ROOTDIR + '/spec'
 end
