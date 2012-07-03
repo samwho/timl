32ea9942212580d53638802faed5292ace91abe8
Sam Rose <samwho@lbak.co.uk>
2012-07-03 14:25:52 +0100

    Added README. Added Travi-CI support.

--- lib/timl/timl.rb
+++ lib/timl/timl.rb
@@ -19,6 +19,23 @@ module Timl
   # Define tags to override existing methods that can mess with the DSL.
   define_tags :p
 
+  # Start a Timl XML string. This is the main method for creating XML with Timl.
+  # It accepts a format as its first parameter, which can either be :flat or
+  # :pretty (defaults to :pretty). The :flat parameter gives you back XML that
+  # has no indentation. The :pretty option runs the generated XML through the
+  # REXML document formatter, which is a little slower but gives great
+  # readability.
+  #
+  # Timl.start then takes a block, which kicks off the DSL. Here's a simple
+  # example:
+  #
+  #   Timl.start :flat do
+  #     p { "This is a paragraph." }
+  #   end
+  #
+  #   #=> <p>This is a paragraph.</p>
+  #
+  # For more example please refer to the README.
   def self.start format = :pretty, &block
     @@out = ""
     module_eval &block
@@ -49,7 +66,23 @@ module Timl
     end
   end
 
-  def self.xml_header
-    @@out << '<?xml version="1.0" encoding="UTF-8" ?>'
+  # Appends:
+  #
+  #   <?xml version="1.0" encoding="UTF-8" ?>
+  #
+  # To the resulting XML file.
+  def self.xml_header options = {}
+    options[:version] = "1.0" unless options[:version]
+    options[:encoding] = "UTF-8" unless options[:encoding]
+    @@out << "<?xml #{parameterize(options)} ?>"
+  end
+
+  # Appends:
+  #
+  #   <!DOCTYPE html>
+  #
+  # To the resulting XML file.
+  def self.html5_doctype
+    @@out << '<!DOCTYPE html>'
   end
 end
